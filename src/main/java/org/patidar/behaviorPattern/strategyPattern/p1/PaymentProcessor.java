0019522dfefb6a05debc2e3d024e729ec9be024d
Patidar <himanshupatidar622@gmail.com>
2026-04-10 23:41:07 +0530

    strategy pattern p1

---
+++ src/main/java/org/patidar/behaviorPattern/strategyPattern/p1/PaymentProcessor.java
@@ -0,0 +1,5 @@
+package org.patidar.behaviorPattern.strategyPattern.p1;
+
+public interface PaymentProcessor {
+    void makePayment(int amount);
+}
--- src/main/java/org/patidar/strategyPattern/p1/CreditCardPayment.java
+++
@@ -1,4 +0,0 @@
-package org.patidar.strategyPattern.p1;
-
-public class CreditCardPayment {
-}
